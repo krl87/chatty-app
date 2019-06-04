28048ba6ad23ede007c207a4294533a0ae56ea01
Kayley <kayley.luftig@gmail.com>
2019-06-04 18:18:08 +0000

    :art: breakdown components into their own jsx file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,36 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx'
+// import Message from './Message.jsx'
+import MessageList from './MessageList.jsx'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar">
-          <a href="/" className="navbar-brand">Chatty</a>
-        </nav>
-        <main className="messages">
-          <div className="message">
-            <span className="message-username">Anonymous1</span>
-            <span className="message-content">I won't be impressed with technology until I can download food.</span>
-          </div>
-          <div className="message system">
-            Anonymous1 changed their name to nomnom.
-          </div>
-        </main>
-        <footer className="chatbar">
-          <input className="chatbar-username" placeholder="Your Name (Optional)" />
-          <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-        </footer>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state =
+      {
+        currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+        messages: [
+          {
+            username: "Bob",
+            content: "Has anyone seen my marbles?",
+          },
+          {
+            username: "Anonymous",
+            content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+          }
+        ]
+      }
   }
+    render() {
+      return (
+        <div>
+          <nav className="navbar">
+            <a href="/" className="navbar-brand">Chatty</a>
+          </nav>
+          <MessageList />
+          <ChatBar />
+        </div>
+      );
+    }
 }
 export default App;
